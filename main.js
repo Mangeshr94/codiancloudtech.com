0c4eeae4b270d05278cfe7b3cdf4ca7b8e350c69
Mangeshr94 <154543044+Mangeshr94@users.noreply.github.com>
2026-03-17 12:37:21 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-links a");
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      console.log(`Navigating to ${link.textContent}`);
+    });
+  });
+});
